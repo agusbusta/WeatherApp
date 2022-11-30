5bbb233970e3f1004931130355075a7982603b5a
agusbusta <agusbus95@gmail.com>
2022-11-30 14:46:57 -0300

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
-const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={chicago}&appid={65f711eef5edeb59a95ececb552d08d5}"
-const xhr = new XMLHttpRequest();
+const obterUbicacion = async()=>{
+    const ciudad = document.getElementById('searcher').value;
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&appid=ff1b03229e0538b78aebdbe11a3be644`)
+    .then(response => response.json())
+    .then(data => obtenerClimaCiudad(JSON.stringify(data)));
+}
 
-function api(){
-    xhr.open("GET",`${apiUrl}`);
+const obtenerClimaCiudad = async (datos) =>{
+    const data = JSON.parse(datos)
+    const lat = data[0].lat
+    const lon = data[0].lon
+
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ff1b03229e0538b78aebdbe11a3be644&units=metric`)
+    .then(response => response.json())
+    .then(data =>console.log(data));
+
+    document.getElementById('temperatura').innerHTML = data.main;
 }
