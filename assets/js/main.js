ef14ae49606dfa701bcf562498e5eec5e44dd0b9
agusbusta <agusbus95@gmail.com>
2022-11-30 15:26:29 -0300

    5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
 const obterUbicacion = async()=>{
     const ciudad = document.getElementById('searcher').value;
+    console.log("CIUDAD",ciudad);
     const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&appid=ff1b03229e0538b78aebdbe11a3be644`)
     .then(response => response.json())
     .then(data => obtenerClimaCiudad(JSON.stringify(data)));
 }
 
 const obtenerClimaCiudad = async (datos) =>{
-    const data = JSON.parse(datos)
-    const lat = data[0].lat
-    const lon = data[0].lon
-
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ff1b03229e0538b78aebdbe11a3be644&units=metric`)
+    const info = JSON.parse(datos)
+    console.log("DATA2",info);
+    const lat = info[0].lat
+    const lon = info[0].lon
+    console.log(lat, lon, info);
+    const res = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=c522ccda026845a4804ab7bf140dd669`)
     .then(response => response.json())
-    .then(data =>console.log(data));
+    .then(data => armaDatos(JSON.stringify(data)));
+}
 
-    document.getElementById('temperatura').innerHTML = data.main;
+const armaDatos = (data) =>{
+    const datos = JSON.parse(data)
+    console.log(datos);
+    document.getElementById("temperatura").innerHTML = datos.data[0].app_temp
 }
